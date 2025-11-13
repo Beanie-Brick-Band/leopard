44ddd8367a3634cf165937fa9842dac40becc4c0
Rachelle DeMan <demanr@mcmaster.ca>
2025-11-13 00:09:40 -0500

    chore: prettier fixes

--- docs/scripts/generate.ts
+++ docs/scripts/generate.ts
@@ -1,10 +1,9 @@
-import { fromVault } from 'fumadocs-obsidian';
+import { fromVault } from "fumadocs-obsidian";
 
 await fromVault({
-  dir: 'vault',
+  dir: "vault",
   out: {
     // you can specify the locations of `/public` & `/content/docs` folder
-    // publicDir: 'public',
-    contentDir: 'content/docs',
+    contentDir: "content/docs",
   },
 });
