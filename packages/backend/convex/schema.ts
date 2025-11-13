558a5591c3b2ef1f96f03189e7988402dd784d58
Timothy Leung <leungt30@mcmaster.ca>
2025-11-13 17:51:59 -0500

    feat(backend): imported db schema add mutations

---
+++ packages/backend/convex/schema.ts
@@ -0,0 +1,45 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  assignments: defineTable({
+    dueDate: v.string(),
+    name: v.string(),
+  }),
+  classrooms: defineTable({
+    assignments: v.array(v.id("assignments")),
+    className: v.string(),
+    metadata: v.string(),
+    ownerId: v.id("users"),
+  }),
+  events: defineTable({
+    changeDetails: v.object({}),
+    changeType: v.string(),
+    timestamp: v.string(),
+    workspaceId: v.id("workspaces"),
+  }),
+  flags: defineTable({
+    description: v.string(),
+    timestamp: v.string(),
+    type: v.string(),
+  }),
+  submissions: defineTable({
+    assignmentId: v.id("assignments"),
+    flags: v.array(v.id("flags")),
+    grade: v.float64(),
+    studentId: v.id("users"),
+    submitted: v.boolean(),
+    workspaceId: v.id("workspaces"),
+  }),
+  users: defineTable({
+    classrooms: v.array(v.id("classrooms")),
+    email: v.string(),
+    name: v.string(),
+    userType: v.string(),
+  }),
+  workspaces: defineTable({ 
+    coderWorkspaceId: v.string(),
+    coderSessionId: v.string(),
+    coderSessionGeneratedTime: v.string()
+   }),
+});
