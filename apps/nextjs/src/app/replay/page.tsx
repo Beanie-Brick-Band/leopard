21ae039ed9b4eb8ea9d6b69d0d029bc0ab6c6dfe
Krish120003 <krish120003@gmail.com>
2026-02-02 23:22:24 -0500

    chore: manual search param override for replay viewer

--- apps/nextjs/src/app/replay/page.tsx
+++ apps/nextjs/src/app/replay/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
+
 import { TextReplayScrubberComponent } from "~/components/scrubber";
 
 export default function ReplayPage() {
   return (
     <div className="px-16 pt-4">
-      <TextReplayScrubberComponent />
+      <Suspense>
+        <TextReplayScrubberComponent />
+      </Suspense>
     </div>
   );
 }
