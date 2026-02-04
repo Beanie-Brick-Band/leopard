b1ee417fa7538ea45a184e695a47b1525a446c0b
Felix Fong <felixfong58@gmail.com>
2026-02-04 17:34:47 -0500

    fix(vscode-extension): configure tests to work with ESM modules

--- apps/vscode-extension/src/extension.ts
+++ apps/vscode-extension/src/extension.ts
@@ -1,11 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as os from "os";
-import { ConvexHttpClient } from "convex/browser";
 import * as vscode from "vscode";
 
 import { api } from "@package/backend/convex/_generated/api";
 import * as WorkspaceEvents from "@package/validators/workspaceEvents";
+
+import { createClient } from "./client";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -19,7 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
   const config = vscode.workspace.getConfiguration("leopard");
   const convexUrl = config.get<string>("convexUrl") ?? "";
 
-  const client = new ConvexHttpClient(convexUrl);
+  const client = createClient(convexUrl);
 
   // Log configuration info
   channel.appendLine(`[INIT] Leopard extension activated`);
